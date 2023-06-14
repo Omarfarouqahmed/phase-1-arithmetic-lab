6088bdf90783b5bcfc9c38ee7f28f2c07ecc5946
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-06-14 23:53:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply  = num1*num2;
+//console.log(multiply)
+
+let random = parseInt(Math.random(2, 1000)*100)
+console.log (random)
+
+let num3 = 25;
+let num4 = 7;
+const mod = num3 % num4;
+console.log(mod)
+
+let set = (1,2,5,9,20);
+const max = Math.max(set)
+console.log(max)
